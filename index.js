13efad98ab8fbc1935d55b3f0495df30c7554de8
Oli Evans <oli@zilla.org.uk>
2016-04-07 19:40:09 +0100

    Save images

---
+++ index.js
@@ -0,0 +1,39 @@
+var fs = require('fs')
+var path = require('path')
+var express = require('express')
+var multiparty = require('multiparty')
+var bodyParser = require('body-parser')
+var format = require('util').format
+var dataUriToBuffer = require('data-uri-to-buffer')
+
+var app = express()
+
+app.use(express.static('users'))
+app.use(bodyParser())
+
+app.get('/', function (req, res) {
+  fs.readdir('users', function (err, files) {
+    if (err || !files) {
+      console.log(err)
+      res.send('no')
+    }
+    res.json(files.filter(function (f) {
+      return f.indexOf('.jpg') > -1
+    }))
+  })
+})
+
+app.post('/', function (req, res, next) {
+  if (!req.body.datauri) return res.send('no data')
+  var buffer = dataUriToBuffer(req.body.datauri)
+  var filename = path.join(__dirname, 'users', Date.now() + '.jpg')
+  fs.writeFile(filename, buffer, {encoding: 'binary'}, function (err) {
+    if (err) return res.send('failed to save')
+    res.send('done')
+    console.log(filename)
+  })
+})
+
+app.listen(1337, function () {
+  console.log('ouroboros...')
+})
